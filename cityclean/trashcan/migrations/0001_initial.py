18f86edd2a6fb4852f16ac7a3ae0b42d17cb2269
hayeon2001 <jhy011108@dgu.ac.kr>
2023-12-20 04:30:33 +0900

    feat: modify table

--- cityclean/trashcan/migrations/0001_initial.py
+++ cityclean/trashcan/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Trash',
+            name='TrashCans',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('address', models.CharField(max_length=255)),
