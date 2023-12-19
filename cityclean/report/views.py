c9649e22c7202075a2f9ea10ccb48bcef51b6483
hayeon2001 <jhy011108@dgu.ac.kr>
2023-12-19 13:24:03 +0900

    feat: user

--- cityclean/report/views.py
+++ cityclean/report/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import trashCans
-from .serializers import TrashCanSerializer, TrashCanListSerializer
+from .serializers import TrashCanSerializer, TrashCanListSerializer, TrashCansListSerializer
 
 class TrashCanCreateView(APIView):
 
@@ -16,6 +16,30 @@ class TrashCanCreateView(APIView):
 
 class TrashCanListView(APIView):
     def get(self, request):
+
+        
         trash_cans_queryset = trashCans.objects.all()
         serializer = TrashCanListSerializer(trash_cans_queryset, many=True)
         return Response(serializer.data)
+
+class TrashCansView(APIView):
+    def get(self, request):
+        user_latitude = float(request.query_params.get('latitude', 0))
+        user_longitude = float(request.query_params.get('longitude', 0))
+        user_location = (user_latitude, user_longitude)
+
+        # 반경 내의 쓰레기통 필터링
+        trash_cans_queryset = trashCans.objects.all()
+        trash_cans_in_radius = [
+            trash_can for trash_can in trash_cans_queryset
+            if self.is_in_radius(user_location, (trash_can.latitude, trash_can.longitude), radius_km=3)
+        ]
+
+        serializer = TrashCansListSerializer(trash_cans_in_radius, many=True)
+        return Response(serializer.data)
+
+    def is_in_radius(self, location1, location2, radius_km):
+        distance = geodesic(location1, location2).kilometers
+        return distance <= radius_km
+
+
