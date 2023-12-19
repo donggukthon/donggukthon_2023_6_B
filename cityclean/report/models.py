1d093eccd58399b795fc879578224de3a68cdac3
hayeon2001 <jhy011108@dgu.ac.kr>
2023-12-19 18:54:58 +0900

    feat: modify trashlist

--- cityclean/report/models.py
+++ cityclean/report/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from user.models import User
 
 class trashCans(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,16 +8,3 @@ class trashCans(models.Model):
     longitude = models.FloatField()
     picture = models.ImageField(upload_to='picture/')
     information = models.TextField()
-    complaincount = models.IntegerField(default=0)
-
-class ComplainCount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    trashCans = models.ForeignKey(trashCans, on_delete=models.CASCADE, related_name='complain_counts')
-
-    def save(self, *args, **kwargs):
-        # 해당 쓰레기통에 대한 신고가 처음인 경우에만 complaincount를 증가
-        if not ComplainCount.objects.filter(user=self.user, trashCans=self.trashCans).exists():
-            self.trashCans.complaincount += 1
-            self.trashCans.save()
-
-        super().save(*args, **kwargs)
